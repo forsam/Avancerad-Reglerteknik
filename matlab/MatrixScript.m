b89434c541c5f804afec57cc4fbc560b4c491133
forsam <samfor-3@student.ltu.se>
2016-11-09 17:52:34 +0100

    Changed some more

--- matlab/MatrixScript.m
+++ matlab/MatrixScript.m
@@ -23,3 +23,23 @@ C = [0,0,1,0];
 G = tf(ss(A,B,C,0));
 [Gnum,Gden] = ss2tf(A,B,C,0);
 
+P1 = 100;
+P2 = 200;
+P3 = 300;
+P4 = 800;
+
+% Gden(2) = C
+% Gden(3) = D
+% Gden(4) = E
+% Gnum(3) = A
+% Gnum(4) = B
+
+Kd = (P1 + P2 + P3 + P4 + Gden(2))/Gnum(3);
+Kp = (P1*P2 + P1*P3 + P1*P4 + P2*P3 + P2*P4 + P3*P4 - Kd*Gnum(4) + Gden(3))/(Gnum(3));
+Ki = (P1*P2*P3 + P1*P2*P4 + P1*P3*P4 + P2*P3*P4 - Kp*Gnum(4) - Gden(4))/(Gnum(3));
+
+D = tf([Kd Kp Ki],[1 0]);
+
+T = D*G/(D*G + 1);
+step(T)
+
