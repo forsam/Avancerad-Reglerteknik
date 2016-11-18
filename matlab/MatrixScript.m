b25c0ae228bf8257e3112e678d13b0b68ae4df83
Arwild <arvidisaksson93@gmail.com>
2016-11-18 14:07:44 +0100

    poles closer to origin

--- matlab/MatrixScript.m
+++ matlab/MatrixScript.m
@@ -22,10 +22,16 @@ C = [0,0,1,0];
 
 G = tf(ss(A,B,C,0));
 [Gnum,Gden] = ss2tf(A,B,C,0);
+%removing cl
+G = tf([Gnum(3) 0],[1 Gden(2) Gden(3) Gden(4)]);
 
-P1 = -1.5+i;
-P2 = -1.5-i;
-P3 = -2;
+%pole placement
+
+[p z] = pzmap(G);
+
+P1 = p(1)+1;
+P2 = p(3)+1;
+P3 = p(3)+2;
 %P4 = 1;
 
 % Gnum(3) = A
@@ -34,22 +40,21 @@ P3 = -2;
 % Gden(3) = D
 % Gden(4) = E
 
-% Kd = (P1 + P2 + P3 + P4 + Gden(2))/Gnum(3);
-% Kp = (P1*P2 + P1*P3 + P1*P4 + P2*P3 + P2*P4 + P3*P4 - Kd*Gnum(4) + Gden(3))/(Gnum(3));
-% Ki = (P1*P2*P3 + P1*P2*P4 + P1*P3*P4 + P2*P3*P4 - Kp*Gnum(4) - Gden(4))/(Gnum(3));
-
 Kd = (-(P1 + P2 + P3) - Gden(2))/Gnum(3);
 Kp = (P1*P2 + P1*P3 + P2*P3 - Gden(3))/(Gnum(3));
 Ki = (-P1*P2*P3 - Gden(4))/(Gnum(3));
 D = tf([Kd Kp Ki],[1 0]);
 
-%G= tf([Gnum(3) 0],[1 Gden(2) Gden(3) Gden(4)])
-T = D*G/(D*G + 1);
+T = feedback(D*G,1);
 T = minreal(T);
-%förfilter
 [Tnum,Tden]=tfdata(T);
-F = tf([Tden{1}(4)],Gnum(3)*[Kd,Kp,Ki]);
-T = minreal(F*T);
+norm = Tden{1}(4)/(Tnum{1}(4));
+T = T*norm;
+[pny zny] = pzmap(T);
+step(T);
+%förfilter
+
+% F = tf([Tden{1}(4)],Gnum(3)*[Kd,Kp,Ki]);
+% T = minreal(F*T);
 
 %opt = stepDataOptions('stepamplitude',pi/180);
-impulse(T)
